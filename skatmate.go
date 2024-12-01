c042a867b9d88940f95e223d1cff92e483195a4c
Niclas Polkow <niclaspolkow@googlemail.com>
2024-12-01 19:26:28 +0100

    feat(chore): Added transformer Skatround -> DocSkatround

--- skatmate.go
+++ skatmate.go
@@ -95,6 +95,76 @@ func (docSkatrunde DocSkatrunde) ToSkatrunde() Skatrunde {
 	return skatrunde
 }
 
+func (spiel Spiel) ToDocSpiel(abrechnungsform Abrechnungsform, isVierSpieler bool, spielNr int, spielerPunkteZuvor []int) DocSpiel {
+	var docSpiel DocSpiel
+	var spielerPunkte []string
+
+	if abrechnungsform == Bierlachs {
+		docSpiel.Punkte = spiel.Punkte
+		//set waiting person
+		if isVierSpieler {
+			spielerPunkte = make([]string, 4)
+			spielerPunkte[spielNr%4] = "*"
+		} else {
+			spielerPunkte = make([]string, 3)
+		}
+		if spiel.Gewonnen {
+			for i, value := range spielerPunkte {
+				if i != spiel.ReizGewinner && value == "" {
+					spielerPunkte[i] = strconv.Itoa(spiel.Punkte + spielerPunkteZuvor[i])
+				}
+			}
+		} else {
+			spielerPunkte[spiel.ReizGewinner] = strconv.Itoa(spiel.Punkte + spielerPunkteZuvor[spiel.ReizGewinner])
+		}
+
+	} else if abrechnungsform == LeipzigerSkat {
+		if spiel.Gewonnen {
+			docSpiel.Punkte = spiel.Punkte
+		} else {
+			docSpiel.Punkte = spiel.Punkte * -1
+		}
+
+		//Set waiting person
+		if isVierSpieler {
+			spielerPunkte = make([]string, 4)
+			spielerPunkte[spielNr%4] = "*"
+		} else {
+			spielerPunkte = make([]string, 3)
+		}
+		spielerPunkte[spiel.ReizGewinner] = strconv.Itoa(docSpiel.Punkte + spielerPunkteZuvor[spiel.ReizGewinner])
+	}
+	docSpiel.SpielerPunkte = spielerPunkte
+	return docSpiel
+}
+
+func (skatrunde Skatrunde) ToDocSkatrunde(abrechnungsform Abrechnungsform) DocSkatrunde {
+	var docSkatrunde DocSkatrunde
+	docSkatrunde.Date = skatrunde.Date
+	docSkatrunde.Spieler = skatrunde.Spieler
+	docSkatrunde.Abrechnungsform = abrechnungsform
+	var isVierSpieler bool
+	if len(skatrunde.Spieler) == 4 {
+		isVierSpieler = true
+	} else {
+		isVierSpieler = false
+	}
+
+	spielerPunkteZuvor := make([]int, 4)
+	for i, spiel := range skatrunde.Spielverlauf {
+		var docSpiel = spiel.ToDocSpiel(abrechnungsform, isVierSpieler, i, spielerPunkteZuvor)
+		for i, spielerWert := range docSpiel.SpielerPunkte {
+			intWert, _ := strconv.Atoi(spielerWert)
+			if intWert != 0 {
+				spielerPunkteZuvor[i] = intWert
+			}
+		}
+		docSkatrunde.Spielverlauf = append(docSkatrunde.Spielverlauf, docSpiel)
+	}
+
+	return docSkatrunde
+}
+
 func ParseSpiel(input []string) (*DocSpiel, error) {
 	var spiel DocSpiel
 	if len(input) == 4 {
@@ -195,5 +265,10 @@ func main() {
 	log.Printf("[Transformed] DocSkatrunde -> Skatrunde: %+v\n", skatrunde)
 
 	//transfer Skatrunde -> DocSkatrunde
+	var bierlachsSkatrunde = skatrunde.ToDocSkatrunde(Bierlachs)
+	log.Printf("[Transformed] Skatrunde -> DocSkatrunde mit Bierlachs : %+v\n", bierlachsSkatrunde)
+
+	var leipzigSkatrunde = skatrunde.ToDocSkatrunde(LeipzigerSkat)
+	log.Printf("[Transformed] Skatrunde -> DocSkatrunde mit LeipzigerSkat : %+v\n", leipzigSkatrunde)
 
 }
